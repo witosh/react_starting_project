e6cf8499348758d3ae1cf99310fac00525466ac2
Witold Sobczak <witold.sobczakk@gmail.com>
2022-02-02 20:10:03 +0100

    Add styles

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,6 +1,10 @@
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
 const AddUser = () => {
 
     return (
+        <Card className={classes.input}>
         <form>
             <label htmlFor="username">Username</label>
             <input id="username" type="text"></input>
@@ -8,6 +12,7 @@ const AddUser = () => {
             <input id="age" type="number"></input>
             <button type="submit">Add User</button>
         </form>
+        </Card>
     );
 }
 
